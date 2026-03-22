f5e5a06aac8a500a1a5d5dfb5925709daeb3b19f
BDrugzy <akimivanov02@gmail.com>
2026-03-23 00:07:24 +0300

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
-const GITHUB_TOKEN = 'ghp_1NamGyrS9CEqE0Q69aF6LGTwI79jfn2ZbtSP';
+const GITHUB_TOKEN = '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';
 const GITHUB_REPO = 'BDrugzy/nikitosearch';
 const PROXY_URL = 'https://github-proxy-2j98.onrender.com';
